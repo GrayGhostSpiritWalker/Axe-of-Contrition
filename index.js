68633101ee38117b2d1849b36d6c4955d6e88d1b
GrayGhostSpiritWalker <grayghostspiritwalker@gmail.com>
2025-08-20 15:22:36 -0700

    define loading sequence

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import yaml from 'https://cdn.jsdelivr.net/npm/yaml@2.8.1/+esm'
 
-fetch ( 'tags.yaml' )
+fetch ( 'years.yaml' )
   .then ( response => response.text () )
   .then ( text => yaml.parse ( text ) )
-  .then ( tags = {
-    
+  .then ( years = {
+    for ( const year of Object.entries ( years ) ) {
+      // generate years list
+      // ...
+      // fetch and display most recent year ( current month )
+    }
+    fetch ( 'tags.yaml' )
+      .then ( response => response.text () )
+      .then ( text => yaml.parse ( text ) )
+      .then ( tags = {
+        // generate search options ( to find other articles )
+      } )
   } )
