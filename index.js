2c7183afaed0407904ede479324a8f8c3df536fd
GrayGhostSpiritWalker <grayghostspiritwalker@gmail.com>
2025-08-20 15:13:30 -0700

    fetch tags yaml

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-fetch ( 'search.json' )
-  .then ( response => response.json () )
-  .then ( json => {
+import yaml from 'https://cdn.jsdelivr.net/npm/yaml@2.8.1/+esm'
+
+fetch ( 'tags.yaml' )
+  .then ( response => response.text () )
+  .then ( text => yaml.parse ( text ) )
+  .then ( tags = {
     
   } )
